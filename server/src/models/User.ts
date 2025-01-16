0cf42ee07ab68a4457237073fd2b2a1ca71cc8dd
sbaird22 <shanebaird30@yahoo.com>
2025-01-15 21:47:15 -0700

    bugs

--- server/src/models/User.ts
+++ server/src/models/User.ts
@@ -1,5 +1,5 @@
 import mongoose, { Schema, Document } from 'mongoose';
-import bcrypt from 'bcrypt';
+import * as bcrypt from 'bcrypt';
 
 // Define the User interface for TypeScript
 export interface IUser extends Document {
